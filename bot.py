721f3636f77dfda3ad8bf33db1781080fecaec0a
dandreis <andreis.derek@gmail.com>
2023-03-15 05:34:32 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 
 import discord
 from dotenv import load_dotenv
+from transformers import pipeline
 
 load_dotenv()
 TOKEN = os.getenv("BOT_KEY_SBOT")
@@ -16,6 +17,8 @@ client = discord.Client(intents=intents)
 with open('skyrim_dialogue.csv') as csvfile:
     data = list(csv.reader(csvfile))
 
+sentiment_pipeline = pipeline("sentiment-analysis")
+
 
 @client.event
 async def on_ready():
@@ -26,7 +29,9 @@ async def on_ready():
 async def on_message(message):
     if str(client.user.id) in message.content:
         rsp = data[random.randint(0, len(data))]
-        print(rsp)
+        print(rsp[0])
+        msg = [message.content]
+        print(sentiment_pipeline(msg))
         await message.channel.send(rsp[0])
     else:
         return
